e97210f7ec9b797b70966e9c86a101cfff3655b8
Abhishek Sinha <3327abhishek@gmail.com>
2020-12-29 23:52:27 +0530

    about me pic upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,4 +22,29 @@ $(document).ready(function(){
         // other options
       });
 
+
+
+      let $certbtns = $('.certificates .btn-cert-grp button');
+    
+      $certbtns.click(function(e){
+          $('.certificates .btn-cert-grp button').removeClass('active');
+          e.target.classList.add('active');
+      
+      let selector =$(e.target).attr('data-filter');
+      $('.certificates .grid').isotope({
+          filter: selector
+      });
+      
+      return false;
+      });
+      
+      $('.certificates .btn-cert-grp #btn2').trigger('click');
+  
+      $('.certificates .grid .test-popup-link').magnificPopup({
+          type: 'image',
+          gallery:{enabled:true}
+          // other options
+        });
+  
+
     })
